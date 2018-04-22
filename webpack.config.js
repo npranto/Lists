7066105ca2ad3ff350462a4c8fa3bb690d1db7d5
Saleem Ahmed <SaleemAhmed@Shaheens-MacBook-Pro.local>
2018-04-22 15:11:32 -0400

    setting up webpack bundler and file structure

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.css$/,
+				exclude: /(node_modules|bower_components)/,				
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+		]
+	}
+}
