0fd21a992c44036eb1e29ed988580439db4e6f18
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-04-25 09:59:48 -0400

    lists-jgf2c2at => On window.onload, set initial activeCategoryId to id of first element in category if category exists, otherwise, set to null

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import { render } from './utils';
-import loadEvents from './events';
+import {
+  loadEventsBeforeDOMRender,
+  loadEventsAfterDOMRender,
+  setActiveCategoryId
+} from './events';
 
 import App from './components/App';
 import './index.css';
 
-render('Lists', App).then(
-  (done, error) => (done ? loadEvents() : console.error(error))
-);
+window.addEventListener('load', () => {
+  setActiveCategoryId();
+
+  loadEventsBeforeDOMRender().then(
+    (eventsLoadedBeforeDOMRender, errorInLoadEventsBeforeDOMRender) => {
+      if (eventsLoadedBeforeDOMRender) {
+        render('Lists', App).then((rendered, errorInRender) => {
+          if (rendered) {
+            loadEventsAfterDOMRender().then(
+              (eventsLoadedAfterDOMRender, errorInLoadEventsAfterDOMRender) => {
+                if (eventsLoadedAfterDOMRender) {
+                  console.log('COOL!');
+                }
+                if (errorInLoadEventsAfterDOMRender) {
+                  console.error(errorInLoadEventsAfterDOMRender);
+                }
+              }
+            );
+          }
+          if (errorInRender) {
+            console.error(errorInRender);
+          }
+        });
+      }
+      if (errorInLoadEventsBeforeDOMRender) {
+        console.error(errorInLoadEventsBeforeDOMRender);
+      }
+    }
+  );
+});
